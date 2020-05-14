52218e8dbc81207e06923fe6e6c2ad6f4dab1f76
storm37000 <6331913+storm37000@users.noreply.github.com>
2020-05-13 20:18:27 -0400

    Update sh_cvars.lua

--- lua/mediaplayer/sh_cvars.lua
+++ lua/mediaplayer/sh_cvars.lua
@@ -20,12 +20,12 @@ MediaPlayer.Cvars.QueueLimit = CreateConVar( "mediaplayer_queue_limit", 64, {
 
 if CLIENT then
 
-	MediaPlayer.Cvars.Resolution	 = CreateClientConVar( "mediaplayer_resolution", 480, true, false )
-	MediaPlayer.Cvars.Audio3D		 = CreateClientConVar( "mediaplayer_3daudio", 1, true, false )
+	MediaPlayer.Cvars.Resolution	 = CreateClientConVar( "mediaplayer_resolution", 4096, true, false )
+	MediaPlayer.Cvars.Audio3D		 = CreateClientConVar( "mediaplayer_3daudio", 0, true, false )
 	MediaPlayer.Cvars.Volume		 = CreateClientConVar( "mediaplayer_volume", 0.15, true, false )
 	MediaPlayer.Cvars.MuteUnfocused	 = 			GetConVar( "snd_mute_losefocus" )
-	MediaPlayer.Cvars.MuteUnfocusedX = CreateClientConVar( "mediaplayer_mute_unfocused", 1, true, false )
+	MediaPlayer.Cvars.MuteUnfocusedX = CreateClientConVar( "mediaplayer_mute_unfocused", 0, true, false )
 	MediaPlayer.Cvars.Fullscreen	 = CreateClientConVar( "mediaplayer_fullscreen", 0, false, false )
-	MediaPlayer.Cvars.DrawThumbnails = CreateClientConVar( "mediaplayer_draw_thumbnails", 0, true, false )
+	MediaPlayer.Cvars.DrawThumbnails = CreateClientConVar( "mediaplayer_draw_thumbnails", 1, true, false )
 
 end
